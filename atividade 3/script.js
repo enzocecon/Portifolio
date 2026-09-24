9109a07d7b9fb4b34fc8b0ee13bf2884b152fa95
enzocecon <enzo.cecon@escola.pr.gov.br>
2026-09-24 08:03:07 -0300

    4

--- atividade 3/script.js
+++ atividade 3/script.js
@@ -3,8 +3,14 @@ let nota2tri;
 let resultado;
 
 function calcular(){
-nota1tri = Number(prompt("qual sua nota do primeiro trimestre"))
-nota2tri = Number(prompt("qual sua nota do segundo trimestre"))
-resultado = 180 - nota1tri + nota2tri;
-alert("a nota que falta para você é "+ resultado)
+nota1tri = Number(prompt("qual sua nota do primeiro trimestre"));
+nota2tri = Number(prompt("qual sua nota do segundo trimestre"));
+resultado = 180 - (nota1tri + nota2tri);
+
+if(resultado = 180){ 
+
+}
+
+
+alert("a nota que falta para você é "+ resultado);
 }
